6d6e2a74945de515fb344c3d9af0406c7d6e24ed
Alexander <alexanders.tischner@outlook.com>
2022-10-20 09:58:13 -0600

    changes to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
-let previousSearch = document.querySelector('.previousSearch');
+const previousSearch = document.querySelector('.previousSearch');
 const searchButton = document.querySelector('.searchButton');
 const time = document.getElementById('time');
 const date = document.getElementById('date');
 const currentWeather = document.getElementById('current-weather-items');
 const dayForcast = document.getElementById('weather-forcast');
+const currentCity = document.getElementById('currentCity');
+const futureForecast = document.getElementById('futureForecast');
+const currentWeatherIcon = document.getElementById('currentWeatherIcon')
+
 
 
 const APIKey = 'c51001d07a06e635ebd124822da632c2';
@@ -25,17 +29,16 @@ setInterval(currentTime, 1000);
 
 
 // connecting to geolocation api for openweather
-let citySearch = function (event) {
+const citySearch = function (event) {
 
     // save search to local storage
     let city = document.getElementById('location').value;
     let towns = JSON.parse(localStorage.getItem('towns',)) || [];
     towns.push(city);
     localStorage.setItem("towns", JSON.stringify(towns));
-// need to do a for loop
-    towns.forEach(createCityEl);
+    towns.forEach(city => createCityEl(city));
 
-
+    
 
     let cityAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
     fetch(cityAPI).then(function (data){
@@ -50,6 +53,7 @@ let citySearch = function (event) {
         fetchFiveDay(response[0])
     }) 
 
+    
 
    
 };
@@ -61,6 +65,13 @@ searchButton.addEventListener('click', citySearch);
      let currentTemp = document.getElementById('current-temp')
      let currentWind = document.getElementById('current-wind-speed')
      let currentHum = document.getElementById('current-humidity')
+     let currentCity = document.getElementById('currentCity')
+    //  let currentDayIcon = document.getElementById('currentWeatherIcon')
+    
+    
+
+    
+
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`
     fetch(apiUrl).then(function (data) {
         if(data.ok) {
@@ -71,10 +82,14 @@ searchButton.addEventListener('click', citySearch);
     }) .then(function (response) {
         console.log(response)
 
-        // taking data from response for current temprature, humidity, and wind speed
+        // taking data from response for current city, temprature, humidity, and wind speed
         currentTemp.textContent = Math.floor(response.main.temp) + `\u00B0 F ` 
         currentHum.textContent = Math.floor(response.main.humidity) + `%`
         currentWind.textContent = Math.floor(response.wind.speed) + ` MPH`
+        currentCity.textContent = response.name
+        
+
+        
             
     })
 
@@ -95,22 +110,29 @@ searchButton.addEventListener('click', citySearch);
     }) .then(function (response) {
         console.log(response)
     })
+
+    // Five Day Forecast
+
+    // function fiveDay(future) {
+    //     let beginingDay = 
+    // }
     
  };
 
- // five day forcast
+ 
 
 
 
 
 // appends list of cities from past searches
-let createCityEl = ({city}) => {
+let createCityEl = (city) => {
     let cityDiv = document.createElement('div');
-    let cityName = document.createElement('h4');
+    let cityNameBtn = document.createElement('button');
 
-    cityName.innerText = 'City: ' + city;
+    cityNameBtn.innerText = 'City: ' + city;
 
-    cityDiv.append(cityName);
+    cityDiv.append(cityNameBtn);
     previousSearch.appendChild(cityDiv);
+    // cityNameBtnBtn.addEventListener('click', citySearch)
 };
 
