43853884e465747fc743a4762c24ef1bca1c184a
Alexander <alexanders.tischner@outlook.com>
2022-10-20 19:19:56 -0600

    added code to save previous searches

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,10 @@ const currentWeatherIcon = document.getElementById('currentWeatherIcon')
 const APIKey = 'c51001d07a06e635ebd124822da632c2';
 // using momentjs for current time, and also date.
 
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+// using momentjs for current time, and also date.
 function currentDate() {
     let todaysDate = moment().format('MMMM Do, YYYY');
     date.textContent = todaysDate;
@@ -36,8 +40,7 @@ const citySearch = function (event) {
     let towns = JSON.parse(localStorage.getItem('towns',)) || [];
     towns.push(city);
     localStorage.setItem("towns", JSON.stringify(towns));
-    towns.forEach(city => createCityEl(city));
-
+    createCityEl(city);
     
 
     let cityAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
@@ -59,6 +62,14 @@ const citySearch = function (event) {
 };
 searchButton.addEventListener('click', citySearch);
 
+let pageLoad = () => {
+    let towns = JSON.parse(localStorage.getItem('towns',)) || [];
+    localStorage.setItem("towns", JSON.stringify(towns));
+    towns.forEach(city => createCityEl(city));
+
+
+}
+
 
 // connecting to current weather api for openweather
  let fetchDaily = function ({lat,lon}) {
@@ -67,11 +78,6 @@ searchButton.addEventListener('click', citySearch);
      let currentHum = document.getElementById('current-humidity')
      let currentCity = document.getElementById('currentCity')
     //  let currentDayIcon = document.getElementById('currentWeatherIcon')
-    
-    
-
-    
-
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`
     fetch(apiUrl).then(function (data) {
         if(data.ok) {
@@ -111,12 +117,61 @@ searchButton.addEventListener('click', citySearch);
         console.log(response)
     })
 
-    // Five Day Forecast
+    // Five Day Forecast card made here instead of in index.html
+    // function fiveDayForecast(forecast) {
+    //     let futureTemp = response.main.temp;
+    //     let futureHumidity = response.main.humidity;
+    //     let futureWind = response.wind.speed;
+    //     let column = document.createElement('div');
+    //     let tile = document.createElement('div');
+    //     let cardB = document.createElement('div');
+    //     let cardTitleDay = document.createElement('h4');
+    //     let windElement = document.createElement('p');
+    //     let tempElement = document.createElement('p');
+    //     let humidityElement = document.createElement('p');
 
-    // function fiveDay(future) {
-    //     let beginingDay = 
-    // }
-    
+    //     column.append(tile);
+    //     tile.append(cardb);
+    //     cardb.append(cardTitleDay, windElement, humidityElement, tempElement);
+
+    //     column.setAttribute('class', 'column-md');
+    //     column.classList.add('five-day-tile');
+    //     tile.setAttribute('class', 'tile bg-primary h-100 text-black');
+    //     cardB.setAttribute('class', 'tile-body p-2');
+    //     cardTitleDay.setAttribute('class', 'tile-title');
+    //     humidityElement.setAttribute('class', 'title-text');
+    //     windElement.setAttribute('class', 'title-text');
+    //     tempElement.setAttribute('class', 'title-text');
+
+    //     cardTitleDay.textContent = dayjs(forecast.dt_txt).format('M/D');
+    //     humidityElement.textContent = `Humidity: ${futureHumidity}` + `%`;
+    //     tempElement.textContent = `Temperature: ${futureTemp}` + `\u00B0 F`;
+    //     windElement.textContent =  `Wind Speed: ${futureWind}` + ` MPH`;
+
+    // futureForecast.append(column);
+
+    };
+
+
+
+//    Five Day Forecast function:
+
+    function displayFutureWeather(futureWeather) {
+        let begin = dayjs().add(1, 'day').startOf('day').unix();
+        let end = dayjs().add(7, 'day').startOf('day').unix();
+        let headColumn = document.createElement('div');
+        let title = document.createElement('h5');
+
+        headColumn.setAttribute('class', 'col-12');
+        title.textContent = 'Future Forecast:';
+        headColumn.append(title);
+
+        futureForecast.innerHTML = '';
+        futureForecast.append(headColumn);
+
+        
+
+
  };
 
  
@@ -129,10 +184,23 @@ let createCityEl = (city) => {
     let cityDiv = document.createElement('div');
     let cityNameBtn = document.createElement('button');
 
-    cityNameBtn.innerText = 'City: ' + city;
+    cityNameBtn.innerText = city;
 
     cityDiv.append(cityNameBtn);
     previousSearch.appendChild(cityDiv);
-    // cityNameBtnBtn.addEventListener('click', citySearch)
+    cityNameBtn.addEventListener('click', historySearch)
 };
 
+let historySearch = (event) => {
+    event.preventDefault();
+    let cityName = event.target.textContent;
+    let searchInput = document.getElementById('location');
+    searchInput.value = cityName;
+
+    
+    citySearch();
+   
+    
+}
+
+pageLoad();
